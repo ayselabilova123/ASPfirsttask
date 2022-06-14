920da11ae60b3660928bafccf5136ce0a5ed8ef9
Aysel Abillova <ayselaa@code.edu.az>
2022-06-14 11:41:59 +0400

    first commit

---
+++ WebApplication1/Controllers/AboutController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    public class AboutController : Controller
+    {
+        private List<int> _numbers;
+        public AboutController()
+        {
+            _numbers = new List<int>()
+            {
+                 3,4,5,6,7,56,100
+            };
+        }
+        public ViewResult CheckMethod()
+        {
+            return new ViewResult();
+        }
+
+        public ViewResult GetNumber(int number)
+        {
+            var isExist = _numbers.Exists(n => n == number);
+            if (isExist)
+            {
+                int myNumber = _numbers.Find(n => n == number);
+                ViewData["Aysel"] = myNumber;
+            }
+            return new ViewResult();
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController
+    {
+        public JsonResult SayHello()
+        {
+            var obj = new
+            {
+                firstname="Aysel",
+                lastname="Abilova",
+                age=21
+            };
+            JsonResult jsonResult = new JsonResult(obj);
+            return jsonResult;
+        }
+
+        public ContentResult SayContentResult()
+        {
+            ContentResult contentResult = new ContentResult();
+
+            contentResult.Content = "Hey you!";
+
+            return contentResult;
+        }
+
+       public ViewResult ViewMethod()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "Test";
+            return viewResult;
+        }
+
+
+
+    }
+
+}
